d40c14a0a2fe987b156195217da90a4100732e05
brihanPenayo <116458646+brihanPenayo@users.noreply.github.com>
2023-01-21 13:11:08 -0300

    obteniendo datos de input

--- form_hooks/src/Components/Form/Form.jsx
+++ form_hooks/src/Components/Form/Form.jsx
@@ -2,9 +2,19 @@ import React from 'react'
 import styles from './Form.module.css';
 
 
+
+
 const Form = props => {
+    const manejoSubmit = (e) => {
+        e.preventDefault();
+        const data = new FormData(e.target);
+        data.get("Confirmar Contrasenha");
+        data.get("Contrasenha");
+    }
+
+
     return (
-        <form className={styles}>
+        <form className={styles} onSubmit={manejoSubmit}>
             {props.children}
         </form>
     )
